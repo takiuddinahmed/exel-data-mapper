24cc248da6e8cc2d7bb9376e178c62a8135fc9eb
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2023-10-22 05:11:22 +0600

    setup basic app with docker

---
+++ server/api/route.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, Response, jsonify
+
+api_bp = Blueprint("api", __name__, url_prefix="/api")
+
+@api_bp.get("/")
+def index()-> Response:
+    return jsonify("Hello world")
